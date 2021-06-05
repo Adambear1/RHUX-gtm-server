4709485ad71b8f0ba6d09a20befd2d0ed5a0426c
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2021-06-05 14:02:35 -0700

    update, basis

--- main.py
+++ main.py
@@ -1,37 +1,37 @@
-import json
-import urllib.request
-import urllib
-import requests
-from urllib import request, parse
-from datetime import datetime, date
-from flask import jsonify, request, make_response, render_template
-from flask_restful import Resource
-from operator import attrgetter
+# import json
+# import urllib.request
+# import urllib
+# import requests
+# from urllib import request, parse
+# from datetime import datetime, date
+# from flask import jsonify, request, make_response, render_template
+# from flask_restful import Resource
+# from operator import attrgetter
 
-class Website(Resource):
-    def __init__(self):
-        self.url = "https://api.harvestapp.com/v2/clients"
+# class Website(Resource):
+#     def __init__(self):
+#         self.url = "https://api.harvestapp.com/v2/clients"
 
-    def get(self):
-        try:
-            request = urllib.request.Request(
-                url=self.url, headers=self.headers)
-            response = urllib.request.urlopen(request, timeout=5)
-            responseBody = response.read().decode("utf-8")
-            res = json.loads(responseBody)
-            print(res)
-            return res
-        except PermissionError as error:
-            raise error
+#     def get(self):
+#         try:
+#             request = urllib.request.Request(
+#                 url=self.url, headers=self.headers)
+#             response = urllib.request.urlopen(request, timeout=5)
+#             responseBody = response.read().decode("utf-8")
+#             res = json.loads(responseBody)
+#             print(res)
+#             return res
+#         except PermissionError as error:
+#             raise error
 
-    def post(self):
-        try:
-            request = urllib.request.Request(
-                url=self.url, headers=self.headers)
-            response = urllib.request.urlopen(request, timeout=5)
-            responseBody = response.read().decode("utf-8")
-            res = json.loads(responseBody)
-            print(res)
-            return res
-        except PermissionError as error:
-            raise error
+#     def post(self):
+#         try:
+#             request = urllib.request.Request(
+#                 url=self.url, headers=self.headers)
+#             response = urllib.request.urlopen(request, timeout=5)
+#             responseBody = response.read().decode("utf-8")
+#             res = json.loads(responseBody)
+#             print(res)
+#             return res
+#         except PermissionError as error:
+#             raise error
