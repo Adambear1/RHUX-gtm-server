ab107acfc360daa288416c3cea7163613fa45965
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2021-06-05 12:12:40 -0700

    update

--- main.py
+++ main.py
@@ -11,8 +11,20 @@ from operator import attrgetter
 class Website(Resource):
     def __init__(self):
         self.url = "https://api.harvestapp.com/v2/clients"
-        
+
     def get(self):
+        try:
+            request = urllib.request.Request(
+                url=self.url, headers=self.headers)
+            response = urllib.request.urlopen(request, timeout=5)
+            responseBody = response.read().decode("utf-8")
+            res = json.loads(responseBody)
+            print(res)
+            return res
+        except PermissionError as error:
+            raise error
+
+    def post(self):
         try:
             request = urllib.request.Request(
                 url=self.url, headers=self.headers)
